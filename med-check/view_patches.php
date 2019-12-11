feb38d2034d8cb302502fa18da47245b4bb1ed55
Le Hou <lehhou@vision.cs.stonybrook.edu>
2019-12-11 11:03:12 -0500

    Bug fixes

--- med-check/view_patches.php
+++ med-check/view_patches.php
@@ -45,29 +45,31 @@ printf("<section class=\"page_container\">\n\n");
 for ($i = 1; $i <= $N_per_page; ++$i) {
     $im_id = $i + $page * $N_per_page;
 
-    $savef = sprintf("./label_files_%s/im_id_%d.txt", $ctype, $im_id);
-    if ($myfile = fopen($savef, 'r'))
-        $class = fread($myfile, 20);
-    else
-        $class = '';
+    if (file_exists(sprintf('%s/%d.png', $folder, $im_id))) {
+        $savef = sprintf("./label_files_%s/im_id_%d.txt", $ctype, $im_id);
+        if ($myfile = fopen($savef, 'r'))
+            $class = fread($myfile, 20);
+        else
+            $class = '';
 
-    printf("<section class=\"instance_container\">\n");
-
-    printf("<div class=\"label_frame\">\n");
-    printf("<form name=\"label\" method=\"POST\" action=label.php?ctype=%s&page=%d&im_id=%d>\n", $ctype, $page, $im_id);
-    printf("<input type=\"submit\" name=\"c1\" value=\"%s\"> Low <br>\n", strpos($class,'c1')!==false ? 'x':'  ');
-    printf("<input type=\"submit\" name=\"c2\" value=\"%s\"> Medium <br>\n", strpos($class,'c2')!==false ? 'x':'  ');
-    printf("<input type=\"submit\" name=\"c3\" value=\"%s\"> High <br>\n", strpos($class,'c3')!==false ? 'x':'  ');
-    printf("<input type=\"submit\" name=\"c4\" value=\"%s\"> Ignore <br>\n", strpos($class,'c4')!==false ? 'x':'  ');
-    printf("<br>\n");
-    printf("<input type=\"submit\" name=\"cr\" value=\"  \"> Clear <br>\n");
-    printf("</form>\n");
-    printf("</div>\n");
-
-    printf("<div class=\"img_frame\">\n");
-    printf("<img height=400px src=\"%s/%d.png\"/>\n", $folder, $im_id);
-    printf("</div>\n");
-    printf("</section>\n\n");
+        printf("<section class=\"instance_container\">\n");
+ 
+        printf("<div class=\"label_frame\">\n");
+        printf("<form name=\"label\" method=\"POST\" action=label.php?ctype=%s&page=%d&im_id=%d>\n", $ctype, $page, $im_id);
+        printf("<input type=\"submit\" name=\"c1\" value=\"%s\"> Low <br>\n", strpos($class,'c1')!==false ? 'x':'  ');
+        printf("<input type=\"submit\" name=\"c2\" value=\"%s\"> Medium <br>\n", strpos($class,'c2')!==false ? 'x':'  ');
+        printf("<input type=\"submit\" name=\"c3\" value=\"%s\"> High <br>\n", strpos($class,'c3')!==false ? 'x':'  ');
+        printf("<input type=\"submit\" name=\"c4\" value=\"%s\"> Ignore <br>\n", strpos($class,'c4')!==false ? 'x':'  ');
+        printf("<br>\n");
+        printf("<input type=\"submit\" name=\"cr\" value=\"  \"> Clear <br>\n");
+        printf("</form>\n");
+        printf("</div>\n");
+ 
+        printf("<div class=\"img_frame\">\n");
+        printf("<img height=400px src=\"%s/%d.png\"/>\n", $folder, $im_id);
+        printf("</div>\n");
+        printf("</section>\n\n");
+    }
 }
 printf("</section>\n");
 ?>
